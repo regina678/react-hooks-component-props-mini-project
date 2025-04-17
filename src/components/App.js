27301d7539800cc73cc234f3d64e9cff1632390e
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-04-17 05:58:21 +0300

    Create App component to display blog data with posts and details

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import blogData from "../data/blog";
 
-console.log(blogData);
-
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <header>
+        <img src={blogData.image} alt="blog logo" />
+        <h1>{blogData.name}</h1>
+        <p>{blogData.about}</p>
+      </header>
+
+      <aside>
+        <h2>About the Blog</h2>
+        <p>{blogData.about}</p>
+      </aside>
+
+      <main>
+        <h1>Blog Posts</h1>
+        {blogData.posts.map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.title}</h2>
+            <small>{post.date}</small>
+            <p>{post.preview}</p>
+            <span>{post.minutes} min read</span>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
